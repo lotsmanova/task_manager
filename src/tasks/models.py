93e687e74facf0c8f29a4e18974b8c971051ddfb
violetta <lotsmanovavioletta@gmail.com>
2024-01-14 12:30:56 +0300

    fixed models

--- src/tasks/models.py
+++ src/tasks/models.py
@@ -1,14 +1,24 @@
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, ForeignKey, TIMESTAMP
+from sqlalchemy import ForeignKey, BigInteger
+from sqlalchemy.orm import Mapped, mapped_column
 from src.database import Base
+from src.tasks.shemas import Task
 
 
 class Tasks(Base):
-    __tablename__ = "tasks"
+    __tablename__ = "task"
 
-    id = Column(Integer, primary_key=True)
-    title = Column(String(80))
-    description = Column(String(200))
-    user_id = Column(ForeignKey("user.id"))
-    date_create = Column(TIMESTAMP, default=datetime.utcnow)
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column()
+    description: Mapped[str] = mapped_column()
+    user_id: Mapped[int] = mapped_column(BigInteger, ForeignKey("user.id", ondelete="CASCADE"), nullable=True)
+    date_create: Mapped[datetime] = mapped_column(default=datetime.utcnow)
 
+    def to_pydantic_model(self) -> Task:
+        return Task(
+            id=self.id,
+            title=self.title,
+            description=self.description,
+            user_id=self.user_id,
+            date_create=self.date_create
+        )
