6ea82384753b5871cb993b1d2653ad1a49f29355
violetta <lotsmanovavioletta@gmail.com>
2024-01-15 21:09:52 +0300

    add structures pattern unit of work

---
+++ src/utils/unitofwork.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+from typing import Type
+from src.database import async_session_maker
+from src.tasks.task_repository import TasksRepository
+
+
+class IUnitOfWork(ABC):
+    tasks: Type[TasksRepository]
+
+    @abstractmethod
+    def __init__(self):
+        ...
+
+    @abstractmethod
+    async def __aenter__(self):
+        ...
+
+    @abstractmethod
+    async def __aexit__(self, *args):
+        ...
+
+    @abstractmethod
+    async def commit(self):
+        ...
+
+    @abstractmethod
+    async def rollback(self):
+        ...
+
+
+class UnitOfWork:
+    def __init__(self):
+        self.session_factory = async_session_maker
+
+    async def __aenter__(self):
+        self.session = self.session_factory()
+
+        self.tasks = TasksRepository(self.session)
+
+    async def __aexit__(self, *args):
+        await self.rollback()
+        await self.session.close()
+
+    async def commit(self):
+        await self.session.commit()
+
+    async def rollback(self):
+        await self.session.rollback()
