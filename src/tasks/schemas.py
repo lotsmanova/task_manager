739b0cb6b80b48fc1401f6a957987733a0f68383
violetta <lotsmanovavioletta@gmail.com>
2024-01-17 20:07:20 +0300

    fixed schemas task

--- src/tasks/schemas.py
+++ src/tasks/schemas.py
@@ -1,25 +1,46 @@
-from pydantic import BaseModel, ConfigDict
+from enum import Enum
 from datetime import datetime
+
+from pydantic import BaseModel, validator
+
+
+class TaskStatus(str, Enum):
+    create = 'create'
+    process = 'process'
+    finish = 'finish'
 
 
 class Task(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
-
     id: int
     title: str
     description: str
+    status: TaskStatus
     user_id: int
     date_create: datetime
+
+    @validator('status')
+    def validate_status(cls, value):
+        return value.value if isinstance(value, TaskStatus) else value
 
 
 class TaskAdd(BaseModel):
 
     title: str
     description: str
+    status: TaskStatus
     user_id: int
+
+    @validator('status')
+    def validate_status(cls, value):
+        return value.value if isinstance(value, TaskStatus) else value
 
 
 class TaskEdit(BaseModel):
 
     title: str
     description: str
+    status: TaskStatus
+
+    @validator('status')
+    def validate_status(cls, value):
+        return value.value if isinstance(value, TaskStatus) else value
