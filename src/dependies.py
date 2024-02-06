c498e27b92269bc94a4223fa76c4946edd38127f
violetta <lotsmanovavioletta@gmail.com>
2024-02-06 16:36:34 +0300

    fixed depend

--- src/dependies.py
+++ src/dependies.py
@@ -1,10 +1,6 @@
 from typing import Annotated
 from fastapi import Depends
-from src.auth.base_config import fastapi_users
-from src.auth.models import User
 from src.utils.unitofwork import IUnitOfWork, UnitOfWork
 
 # Depend for app
 UOWDep = Annotated[IUnitOfWork, Depends(UnitOfWork)]
-
-User_ver = Annotated[User, Depends(fastapi_users.current_user(active=True))]
