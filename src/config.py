cdb241b77bf0b8c153d72602833605576dcee454
violetta <lotsmanovavioletta@gmail.com>
2024-02-06 16:36:16 +0300

    fixed config and main

--- src/config.py
+++ src/config.py
@@ -6,12 +6,15 @@ load_dotenv()
 # setting for DB
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASS")
+DB_NAME = os.environ.get("POSTGRES_DB")
+DB_USER = os.environ.get("POSTGRES_USER")
+DB_PASS = os.environ.get("POSTGRES_PASSWORD")
 
 # setting for auth
 SECRET_AUTH = os.environ.get("SECRET_AUTH")
+ALGORITHM = os.environ.get("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
+SALT = os.environ.get("SALT")
 
 # setting for test
 DB_HOST_TEST = os.environ.get("DB_HOST_TEST")
